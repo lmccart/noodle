a3daeca2c119f649bc91f9cb1af0abc01762277e
lauren mccarthy <laurmccarthy@gmail.com>
2014-01-07 15:14:08 -0800

    checking for hits

--- intel.js
+++ intel.js
@@ -1,8 +1,10 @@
 
 module.exports = function(settings) {
 
+	var parseString = require('xml2js').parseString;
+
 	var creds = require('./config'),
-			mturk  = require('./mturk')({creds: creds, sandbox: true}),
+			mturk  = require('./mturk')({creds: creds, sandbox: false}),
 			fs = require('fs'),
 			util = require("util");
 
@@ -10,7 +12,7 @@ module.exports = function(settings) {
 	  Title: "Mturk Nodejs module RegisterHITType test"
 	  , Keywords: "keyword1, keyword2, keyword3" 
 	  , Description: "Test description"
-	  , Reward: {Amount: 1.0, CurrencyCode: "USD"}
+	  , Reward: {Amount: 0.01, CurrencyCode: "USD"}
 	  , AssignmentDurationInSeconds: 3600
 	  , AutoApprovalDelayInSeconds: 3600
 	  , QualificationRequirement: [mturk.QualificationRequirements.Adults]
@@ -52,28 +54,45 @@ module.exports = function(settings) {
 
 	intel.checkForHits = function() {
 		mturk.GetReviewableHITs({}, function(err, result){
-      var hits = (result.HIT instanceof Array) ? result.HIT : [result.HIT];
-      hits.forEach(function(HIT){
-      	if (HIT) {
+			if (result) {
+	      var hits = (result.HIT instanceof Array) ? result.HIT : [result.HIT];
+	      hits.forEach(function(HIT){
 	      	console.log(HIT);
 	        // For each reviewable HIT, get the assignments that have been submitted
 	        mturk.GetAssignmentsForHIT({ "HITId": HIT.HITId }, function(err, result){
 	          var assignments = (result.Assignment instanceof Array) ? result.Assignment : [result.Assignment];
 	          assignments.forEach(function(assignment){
-	            console.log( util.inspect(assignment) );
+	          	if (assignment.AssignmentStatus === "Submitted") {
 
-	            // Here you could do
-	            // mturk.ApproveAssignment({"AssignmentId": assignment.AssignmentId, "RequesterFeedback": "Great work!"}, function(err, id){ 
-	                    // Now assignment "id" is approved!
-	            // });
+		            // mturk.ApproveAssignment({"AssignmentId": assignment.AssignmentId, "RequesterFeedback": "Great work!"}, function(err, id){ 
+		            // 	console.log("approved "+assignment.AssignmentId);
+		            // });
+
+								parseString(assignment.Answer, function (err, result) {
+										var answer = result.QuestionFormAnswers.Answer[0].FreeText[0];
+								    console.dir(answer);
+
+								    // act on the result
+								    intel.actOnResponse({"hit": HIT.HITId, "response": answer});
+
+								});
+
+	        		}
+
 	          });
 	        });
-	      }
-      });
+		      
+	      });
+			}
 		});
 	};
 
-	var checkInterval = setInterval(intel.checkForHits, 5000);
+	intel.actOnResponse = function(resp) {
+		console.log(resp);
+	};
+
+	//var checkInterval = setInterval(intel.checkForHits, 5000);
+	intel.checkForHits();
 
 	return intel;
 };
