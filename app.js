ac66754c47220a49547bed31e715a6e44e0a1ebe
lauren mccarthy <laurmccarthy@gmail.com>
2014-01-06 16:21:28 -0800

    formatting

--- app.js
+++ app.js
@@ -40,37 +40,37 @@ var fs = require('fs');
 
 
 var RegisterHITTypeOptions = { 
-        Title: "Mturk Nodejs module RegisterHITType test"
-        , Keywords: "keyword1, keyword2, keyword3" 
-        , Description: "Test description"
-        , Reward: {Amount: 1.0, CurrencyCode: "USD"}
-        , AssignmentDurationInSeconds: 3600
-        , AutoApprovalDelayInSeconds: 3600
-        , QualificationRequirement: [mturk.QualificationRequirements.Adults]
+  Title: "Mturk Nodejs module RegisterHITType test"
+  , Keywords: "keyword1, keyword2, keyword3" 
+  , Description: "Test description"
+  , Reward: {Amount: 1.0, CurrencyCode: "USD"}
+  , AssignmentDurationInSeconds: 3600
+  , AutoApprovalDelayInSeconds: 3600
+  , QualificationRequirement: [mturk.QualificationRequirements.Adults]
 };
 
 
 // Step 1: First we have to create a HITTypeId
 mturk.RegisterHITType(RegisterHITTypeOptions, function(err, HITTypeId){
-        if (err) throw err;
+  if (err) throw err;
 
-        fs.readFile("./data/QuestionForm.xml", 'utf8', function(err, questionXML) {
-                if (err) throw err;
+  fs.readFile("./data/QuestionForm.xml", 'utf8', function(err, questionXML) {
+    if (err) throw err;
 
 
-                var CreateHITOptions = {
-                        'HITTypeId': HITTypeId
-                        , 'Question': questionXML
-                        , 'LifetimeInSeconds': 60 * 20  // How long should the assignment last?
-                        , 'MaxAssignments': 1
-                };
+    var CreateHITOptions = {
+      'HITTypeId': HITTypeId
+      , 'Question': questionXML
+      , 'LifetimeInSeconds': 60 * 20  // How long should the assignment last?
+      , 'MaxAssignments': 1
+    };
 
-                // Step 2: Now create the HIT itself.
-                mturk.CreateHIT(CreateHITOptions, function(err, HITId){
-                        if (err) throw err;
-                        console.log("Created HIT "+HITId);
-                });  // mturk.CreateHIT
-        });
+    // Step 2: Now create the HIT itself.
+    mturk.CreateHIT(CreateHITOptions, function(err, HITId){
+      if (err) throw err;
+      console.log("Created HIT "+HITId);
+    });  // mturk.CreateHIT
+  });
 });
 
 
