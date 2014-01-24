dec370ba70f5f09f50bebae6f5a1b08dda5148cb
lauren mccarthy <laurmccarthy@gmail.com>
2014-01-24 15:32:37 -0800

    taking out pyaudio for now

--- python/audio.py
+++ python/audio.py
@@ -1,10 +1,10 @@
-import pyaudio
+#import pyaudio
 import struct
 import math
 
 
 INITIAL_TAP_THRESHOLD = 0.025
-FORMAT = pyaudio.paInt16 
+#FORMAT = pyaudio.paInt16 
 SHORT_NORMALIZE = (1.0/32768.0)
 CHANNELS = 2
 RATE = 44100  
@@ -44,12 +44,12 @@ class Audio(object):
     self.running = False;
     self.registered_events = [];
 
-    self.pa = pyaudio.PyAudio()
-    self.stream = self.open_mic_stream()
-    self.tap_threshold = INITIAL_TAP_THRESHOLD
-    self.noisycount = MAX_TAP_BLOCKS+1 
-    self.quietcount = 0 
-    self.errorcount = 0
+    # self.pa = pyaudio.PyAudio()
+    # self.stream = self.open_mic_stream()
+    # self.tap_threshold = INITIAL_TAP_THRESHOLD
+    # self.noisycount = MAX_TAP_BLOCKS+1 
+    # self.quietcount = 0 
+    # self.errorcount = 0
 
 
 
@@ -63,65 +63,65 @@ class Audio(object):
     self.running = False;
     self.stream.close()
 
-  def find_input_device(self):
-    device_index = None            
-    for i in range( self.pa.get_device_count() ):     
-      devinfo = self.pa.get_device_info_by_index(i)   
-      print( "Device %d: %s"%(i,devinfo["name"]) )
+  # def find_input_device(self):
+  #   device_index = None            
+  #   for i in range( self.pa.get_device_count() ):     
+  #     devinfo = self.pa.get_device_info_by_index(i)   
+  #     print( "Device %d: %s"%(i,devinfo["name"]) )
 
-      for keyword in ["mic","input"]:
-        if keyword in devinfo["name"].lower():
-          print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
-          device_index = i
-          return device_index
+  #     for keyword in ["mic","input"]:
+  #       if keyword in devinfo["name"].lower():
+  #         print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+  #         device_index = i
+  #         return device_index
 
-    if device_index == None:
-      print( "No preferred input found; using default input device." )
+  #   if device_index == None:
+  #     print( "No preferred input found; using default input device." )
 
-    return device_index
+  #   return device_index
 
-  def open_mic_stream( self ):
-    device_index = self.find_input_device()
+  # def open_mic_stream( self ):
+  #   device_index = self.find_input_device()
 
-    stream = self.pa.open(   format = FORMAT,
-                             channels = CHANNELS,
-                             rate = RATE,
-                             input = True,
-                             input_device_index = device_index,
-                             frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+  #   stream = self.pa.open(   format = FORMAT,
+  #                            channels = CHANNELS,
+  #                            rate = RATE,
+  #                            input = True,
+  #                            input_device_index = device_index,
+  #                            frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
 
-    return stream
+  #   return stream
 
-  def tapDetected(self):
-    print "Tap!"
-    # socketIO.emit('event', {'mode':'audio', 'type':'tap'})
-    # socketIO.wait(seconds=1)
+  # def tapDetected(self):
+  #   print "Tap!"
+  #   # socketIO.emit('event', {'mode':'audio', 'type':'tap'})
+  #   # socketIO.wait(seconds=1)
 
-  def listen(self):
-    try:
-      block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
-    except IOError, e:
-      # dammit. 
-      self.errorcount += 1
-      print( "(%d) Error recording: %s"%(self.errorcount,e) )
-      self.noisycount = 1
-      return
+  # def listen(self):
+  #   try:
+  #     block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+  #   except IOError, e:
+  #     # dammit. 
+  #     self.errorcount += 1
+  #     print( "(%d) Error recording: %s"%(self.errorcount,e) )
+  #     self.noisycount = 1
+  #     return
 
-    amplitude = get_rms( block )
-    if amplitude > self.tap_threshold:
-      # noisy block
-      self.quietcount = 0
-      self.noisycount += 1
-      if self.noisycount > OVERSENSITIVE:
-        # turn down the sensitivity
-        self.tap_threshold *= 1.1
-    else:            
-      # quiet block.
+  #   amplitude = get_rms( block )
+  #   if amplitude > self.tap_threshold:
+  #     # noisy block
+  #     self.quietcount = 0
+  #     self.noisycount += 1
+  #     if self.noisycount > OVERSENSITIVE:
+  #       # turn down the sensitivity
+  #       self.tap_threshold *= 1.1
+  #   else:            
+  #     # quiet block.
 
-      if 1 <= self.noisycount <= MAX_TAP_BLOCKS:
-        self.tapDetected()
-      self.noisycount = 0
-      self.quietcount += 1
-      if self.quietcount > UNDERSENSITIVE:
-        # turn up the sensitivity
-        self.tap_threshold *= 0.9
+  #     if 1 <= self.noisycount <= MAX_TAP_BLOCKS:
+  #       self.tapDetected()
+  #     self.noisycount = 0
+  #     self.quietcount += 1
+  #     if self.quietcount > UNDERSENSITIVE:
+  #       # turn up the sensitivity
+  #       self.tap_threshold *= 0.9
