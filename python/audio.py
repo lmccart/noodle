4e22f3ecf09509ea9469f56ba4f5d5dfe10630e5
lauren mccarthy <laurmccarthy@gmail.com>
2014-01-28 01:12:19 +0000

    test

--- python/audio.py
+++ python/audio.py
@@ -4,9 +4,7 @@ import math
 import wave
 import sys
 from subprocess import call
-
-import urllib, os
-import subprocess
+import pyttsx
 
 CHUNK = 512
 SHORT_NORMALIZE = (1.0/32768.0)
@@ -48,22 +46,12 @@ class Audio(object):
     print 'audio: fire ', event, i
     if event[0] == 'play':
       self.play(event[1])
-
-
-  def getGoogleSpeechURL(self, phrase):
-      googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
-      parameters = {'q': phrase}
-      data = urllib.urlencode(parameters)
-      googleTranslateURL = "%s%s" % (googleTranslateURL,data)
-      return googleTranslateURL
-
-  def speakSpeechFromText(self, phrase):
-      #googleSpeechURL = self.getGoogleSpeechURL(phrase)
-      googleSpeechURL = "http://translate.google.com/translate_tts?tl=english&q=" + phrase
-      subprocess.call(["mplayer",googleSpeechURL], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
  
-      #self.downloadFile(googleSpeechURL,"tts.mp3")
-      #os.system("mplayer tts.mp3 -af extrastereo=0 &")
+  def speak(self, phrase):
+    print 'speak ', phrase
+    engine = pyttsx.init()
+    engine.say(phrase)
+    engine.runAndWait()
 
   def play(self, f):
     print 'audio: playing ', f
